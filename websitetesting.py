8ef874eeb788eb5a640fddcebc8456fa5a996551
Le Ngoc Canh <93439144+jamesle001@users.noreply.github.com>
2022-10-11 19:34:53 +0800

    Update websitetesting.py

--- websitetesting.py
+++ websitetesting.py
@@ -7,9 +7,16 @@ import pickle as pk
 from PIL import Image 
 
 loaded_model = pk.load(open('final_model.sav', 'rb'))
-def prediction(image_name):
-    img_list = list()
-    image = cv2.imread(image_name)
+
+st.header("Wavelength Prediction Application")
+
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    image = cv2.imread(image)
     image = cv2.resize(image, (70, 70))
     image = image/255.0
     img_list.append(image)
@@ -20,14 +27,4 @@ def prediction(image_name):
     y_pred = loaded_model.predict(img_pca)
     print("Prediction: {}".format(y_pred))
     print("Real label:", image_name)
-
-st.header("Wavelength Prediction Application")
-
-uploaded_file = st.file_uploader("Choose an image...", type="jpg")
-if uploaded_file is not None:
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.write("Classifying...")
-    label = prediction(uploaded_file)
-    st.write('%s (%.2f%%)' % (label[1], label[2]*100))
+    
