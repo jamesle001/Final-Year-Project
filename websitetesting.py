e5d7eb1ac10a9241ac8016dd7fd954a737e89957
Le Ngoc Canh <93439144+jamesle001@users.noreply.github.com>
2022-10-11 19:08:55 +0800

    Update websitetesting.py

--- websitetesting.py
+++ websitetesting.py
@@ -1,13 +1,32 @@
 import streamlit as st
+import pandas as pd
+from sklearn.decomposition import PCA 
+import numpy as np
+import cv2
+import pickle as pk
 
-st.header("Fish Weight Prediction App")
-st.text_input("Enter your Name: ", key="name")
+loaded_model = pk.load(open('final_model.sav', 'rb'))
+def prediction(image_name):
+    img_list = list()
+    image = cv2.imread(image_name)
+    image = cv2.resize(image, (70, 70))
+    image = image/255.0
+    img_list.append(image)
+    img = np.asarray(img_list)
+    img = img.reshape(img.shape[0],img.shape[1]*img.shape[1]*img.shape[3])
+    pca_reload = pk.load(open("pca.pkl",'rb'))
+    img_pca=pca_reload.transform(img)
+    y_pred = loaded_model.predict(img_pca)
+    print("Prediction: {}".format(y_pred))
+    print("Real label:", image_name)
 
-st.subheader("Please select relevant features of your fish!")
+st.header("Wavelength Prediction Application")
 
-
-input_Length1 = st.slider('Vertical length(cm)', 0.0, 100.0, 1.0)
-input_Length2 = st.slider('Diagonal length(cm)', 0.0, 100.0, 1.0)
-input_Length3 = st.slider('Cross length(cm)', 0.0, 100.0, 1.0)
-input_Height = st.slider('Height(cm)', 0.0, 100.0, 1.0)
-input_Width = st.slider('Diagonal width(cm)', 0.0, 100.0, 1.0)
+uploaded_file = st.file_uploader("Choose an image...", type="jpg")
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    label = predicttion(uploaded_file)
+    st.write('%s (%.2f%%)' % (label[1], label[2]*100))
