834e86cc262adbcbee6b3e40a3393f122ce900ae
Le Ngoc Canh <93439144+jamesle001@users.noreply.github.com>
2022-10-11 20:00:38 +0800

    Update websitetesting.py

--- websitetesting.py
+++ websitetesting.py
@@ -20,6 +20,7 @@ if uploaded_file is not None:
     image = Image.open(uploaded_file)
     image.resize((70,70))
     image = np.array(image)
+    st.write(image)
     image = image.reshape(1,14700)
     image = image/255.0
     st.write(image.shape)
